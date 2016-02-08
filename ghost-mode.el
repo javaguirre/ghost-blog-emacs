58f7f5d7835c4bb472f96f1d24f25d800eb62a97
Javier Aguirre <contacto@javaguirre.net>
2016-02-08 16:14:37 +0100

    Creation of the main file

---
+++ ghost-mode.el
@@ -0,0 +1,21 @@
+;;; ghost-mode.el --- A mode to manage Ghost blog
+;;
+
+;; Author: Javier Aguirre <hello@javaguirre.net>
+;; Maintainer: Javier Aguirre <hello@javaguirre.net>
+;; Created: 10 Feb 2016
+;; Keywords: ghost
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;;
+;; This is a major mode to manage Ghost blogs
+
+;;; Code:
+;;
+(require 'url)
+(require 'json)
+
+(provide 'ghost-mode)
+;;; ghost-mode.el ends here

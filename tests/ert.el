75203c903948cef309756f0bf702dd8f661faf32
Javier Aguirre <contacto@javaguirre.net>
2016-03-29 15:02:10 +0200

    get post list endpoint unittesting

--- tests/ert.el
+++ tests/ert.el
@@ -9,5 +9,14 @@
   (should
    (equal "---\n\ntitle: New title\nslug: /new-title\n\n---\n\nNew post" (buffer-string))))
 
+(ert-deftest get-post-list-endpoint ()
+  (should
+   (equal "/posts/?limit=10" (ghost-mode--get-post-list-endpoint))))
+
+(ert-deftest get-post-list-endpoint-limit-changed ()
+  (setq ghost-mode-post-list-limit 2)
+  (should
+   (equal "/posts/?limit=2" (ghost-mode--get-post-list-endpoint))))
+
 (provide 'ert)
 ;;; ert.el ends here

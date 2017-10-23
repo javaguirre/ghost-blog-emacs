71b358643cc9a2db1bf752281ff94aba9b59e4cc
Javier Aguirre <contacto@javaguirre.net>
2017-10-23 09:42:00 +0200

    Added GPLv3 license

--- ghost-blog.el
+++ ghost-blog.el
@@ -17,6 +17,21 @@
 ;; posts and opening them.Edit and create post is implemented
 ;; but won't work until the Ghost team opens that part of the API
 
+;;; LICENSE
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Code:
 ;;
 (require 'url)
